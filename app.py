a7c2bc1b94ace660bdcaac621d4e1bc9994f1217
jpd-123 <prasadjurra6@gmail.com>
2026-08-16 07:44:36 +0530

    Enable serving static files in Flask application
    Added static file serving functionality to the Flask app.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder='.')
+# Root folder నే Templates & Static Folder గా ఉపయోగిస్తున్నాం
+app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Static ఫైళ్లు (logo.png.jpg లాంటివి) లోడ్ అవ్వడానికి
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
